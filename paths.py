58e40b10668ccddb4070c881739a94e6c452ddc1
Filip Nowakowicz <filip.nowakowicz@gmail.com>
2026-08-10 00:27:57 +0200

    Bump version to 2.0.0

--- paths.py
+++ paths.py
@@ -13,7 +13,7 @@ can sit at the bottom of the import graph and be safely imported everywhere.
 """
 from pathlib import Path
 
-__version__ = "1.3.0"
+__version__ = "2.0.0"
 
 CONFIG_DIR = Path.home() / ".config" / "info-kierowca-notifier"
 CONFIG_FILE = CONFIG_DIR / "config.json"
